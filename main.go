9302f74de38d1d93394c443a4110bf4ae93c1dec
joe-tripodi <joe@flowofcode.com>
2025-02-20 10:12:54 +1030

    refactor

--- main.go
+++ main.go
@@ -7,17 +7,20 @@ import (
 	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
-	apiCfg := &apiConfig{
-		fileServerHits: atomic.Int32{},
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
 	}
 
 	mux := http.NewServeMux()
-	fileServerHandler := http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServerHandler))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("/healthz", handlerReadiness)
 	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("/reset", apiCfg.handlerReset)
@@ -31,31 +34,15 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
-type apiConfig struct {
-	fileServerHits atomic.Int32
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("incrementing server hits")
-		cfg.fileServerHits.Add(1)
+		cfg.fileserverHits.Add(1)
 		next.ServeHTTP(w, r)
 	})
-}
-
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	text := fmt.Sprintf("Hits: %v", cfg.fileServerHits.Load())
-	w.Write([]byte(text))
-}
-
-func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-	cfg.fileServerHits.Swap(0)
-}
-
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
