a8dbfc7a3cf69fe505c5d762fb6bd7385064f733
joe-tripodi <joe@flowofcode.com>
2025-02-20 15:39:44 +1030

    refactoring

--- validation.go
+++ validation.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 )
 
@@ -34,34 +33,4 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 		Valid: true,
 	})
 
-}
-
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	dat, err := json.Marshal(payload)
-	if err != nil {
-		log.Printf("Error marshalling JSON: %s", err)
-		w.WriteHeader(500)
-		return
-	}
-	w.WriteHeader(code)
-	w.Write(dat)
-}
-
-func respondWithError(w http.ResponseWriter, code int, msg string, err error) {
-	if err != nil {
-		log.Println(err)
-	}
-
-	if code > 499 {
-		log.Printf("Responding with 5XX error: %s", msg)
-	}
-
-	type errResponse struct {
-		Error string `json:"error"`
-	}
-
-	respondWithJSON(w, code, errResponse{
-		Error: msg,
-	})
 }
