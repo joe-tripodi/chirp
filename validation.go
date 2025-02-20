d2a02b004aeb33cf24098b02a367733ec85807c1
joe-tripodi <joe@flowofcode.com>
2025-02-20 15:35:41 +1030

    refactor

--- validation.go
+++ validation.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -12,27 +11,57 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 		Body string `json:"body"`
 	}
 
+	type returnVals struct {
+		Valid bool `json:"valid"`
+	}
+
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
 	err := decoder.Decode(&params)
 	if err != nil {
-		log.Printf("failed to parse json: %s", err)
-		errMsg := fmt.Sprintf(`{"error": "%s"}`, err)
-		w.WriteHeader(500)
-		w.Write([]byte(errMsg))
+		respondWithError(w, http.StatusInternalServerError, "couldn't decode parameters", err)
 		return
 	}
 
-	if len(params.Body) > 140 {
-		log.Println("Invalid tweet length, larger than 140")
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(400)
-		w.Write([]byte(`{"valid": false}`))
+	const maxChirpLength = 140
+
+	if len(params.Body) > maxChirpLength {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
 		return
 	}
 
-	log.Println("Valid tweet")
+	respondWithJSON(w, http.StatusOK, returnVals{
+		Valid: true,
+	})
+
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"valid": true}`))
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string, err error) {
+	if err != nil {
+		log.Println(err)
+	}
+
+	if code > 499 {
+		log.Printf("Responding with 5XX error: %s", msg)
+	}
+
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJSON(w, code, errResponse{
+		Error: msg,
+	})
 }
