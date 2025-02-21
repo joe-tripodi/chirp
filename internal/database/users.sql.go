71f62edac5522831e23ea079faf5250948568d04
joe-tripodi <joe@flowofcode.com>
2025-02-21 18:31:52 +1030

    update updated_at for user

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -73,7 +73,8 @@ const updateUser = `-- name: UpdateUser :one
 UPDATE users
 SET
   hashed_password = $1,
-  email = $2
+  email = $2,
+  updated_at = NOW()
 WHERE id = $3
 RETURNING id, created_at, updated_at, email, hashed_password
 `
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -21,6 +21,7 @@ LIMIT 1;
 UPDATE users
 SET
   hashed_password = $1,
-  email = $2
+  email = $2,
+  updated_at = NOW()
 WHERE id = $3
 RETURNING *;
